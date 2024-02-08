a1ad6376916f73daf25bc54e03d2f2748515dcb1
Judieian <90787593+PrathamPawale@users.noreply.github.com>
2024-02-08 07:44:49 +0530

    Add files via upload

---
+++ tru.py
@@ -0,0 +1,89 @@
+import pymongo
+import pandas as pd
+from bson import ObjectId
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.cluster import KMeans
+import streamlit as st
+import pydeck as pdk
+import datetime
+
+st.header('3D Visualization of Location Clusters')
+
+MONGO_URL = "mongodb+srv://upendrataral21:safety123@cluster0.x0pyhch.mongodb.net/avert?retryWrites=true&w=majority"
+COLLECTION_NAME = "avert_data"
+connection = pymongo.MongoClient(MONGO_URL)
+
+database = connection.get_database()
+collection = database[COLLECTION_NAME]
+print("Connected to MongoDB Atlas")
+
+
+#database connection done=======================
+
+#collecting and processing data to fetch ........
+all_data = collection.find()
+lat=[]
+long=[]
+for data in all_data:
+    lat.append(data["latitude"])
+    long.append(data["longitude"])
+
+
+daf=pd.DataFrame({"lat":lat,"long":long})
+X=np.array(daf)
+cls_no=5  ## no. of clusters
+kmeans=KMeans(n_clusters=cls_no)
+kmeans.fit(X)
+
+centroids = kmeans.cluster_centers_
+labels = kmeans.labels_
+cluster_counts = np.zeros(num_clusters, dtype=int)
+
+for label in labels:
+    cluster_counts[label] += 1
+
+cluster_info = []
+
+for cluster_label, count, centroid in zip(range(num_clusters), cluster_counts, centroids):
+    cluster_info.append([count,centroid.tolist()])
+
+cl_inf=pd.DataFrame(cluster_info)  #converted data into datframe
+
+
+
+x = datetime.datetime.now()
+st.subheader("Data Updated on : ",x)
+#####add feature to --like count ,date etc
+
+
+layer = pdk.Layer(
+    "ScatterplotLayer",
+    cl_inf,
+    pickable=True,
+    opacity=0.8,
+    stroked=True,
+    filled=True,
+    radius_scale=300,
+    radius_min_pixels=5,
+    radius_max_pixels=15,
+    line_width_min_pixels=1,
+    get_position=centroids,
+    #get_radius="exits_radius",
+    get_fill_color=[200, 140, 120],
+    get_line_color=[0, 0, 0],
+    
+)
+
+
+tooltip = {
+    "html": "<b>count: ,{0}</b>",
+    "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
+}
+
+view_state = pdk.ViewState(longitude=78.348516,
+    latitude=22.824289, zoom=10, bearing=0, pitch=0)
+
+r = pdk.Deck(layers=[layer],tooltip=tooltip,initial_view_state=view_state)
+#r.to_html("scatterplot_layer.html")
+st.write(r)
