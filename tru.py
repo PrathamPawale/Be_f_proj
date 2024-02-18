91181a0ad9429bff8595ca024013a6dfe289af01
Judieian <90787593+PrathamPawale@users.noreply.github.com>
2024-02-18 20:23:14 +0530

    Update tru.py

--- tru.py
+++ tru.py
@@ -35,56 +35,9 @@ for data in all_data:
 daf=pd.DataFrame({"lat":lat,"long":long,"sos":sos})
 Xs=np.array(daf[daf["sos"]=="true"][["lat","long"]])
 if st.button("Live S.O.S"):
-    layer01 = pdk.Layer(
+    layer = pdk.Layer(
     "ScatterplotLayer",
-    Xs,
-    pickable=True,
-    opacity=0.8,
-    stroked=True,
-    filled=True,
-    radius_scale=6,
-    radius_min_pixels=1,
-    radius_max_pixels=100,
-    line_width_min_pixels=1,
-    get_position=Xs,
-    get_radius="exits_radius",
-    get_fill_color=[255, 140, 0],
-    get_line_color=[0, 0, 0],)
-    view_state = pdk.ViewState(latitude=37.7749295, longitude=-122.4194155, zoom=10, bearing=0, pitch=0)
-    r01 = pdk.Deck(layers=[layer01], initial_view_state=view_state)  #tooltip={"text": "{name}\n{address}"}
-    #r.to_html("scatterplot_layer.html")
-    st.write(r01)
-
-
-
-cls_no=5  ## no. of clusters
-kmeans=KMeans(n_clusters=cls_no)
-kmeans.fit(X)
-
-centroids = kmeans.cluster_centers_
-labels = kmeans.labels_
-cluster_counts = np.zeros(cls_no, dtype=int)
-
-for label in labels:
-    cluster_counts[label] += 1
-
-cluster_info = []
-
-for cluster_label, count, centroid in zip(range(cls_no), cluster_counts, centroids):
-    cluster_info.append([count,centroid.tolist()[1],centroid.tolist()[0]])
-
-cl_inf=pd.DataFrame(cluster_info)  #converted data into datframe
-
-
-
-xt = datetime.now()
-st.subheader("Data Updated on : ",xt)
-#####add feature to --like count ,date etc
-
-
-layer = pdk.Layer(
-    "ScatterplotLayer",
-    cl_inf,
+    daf,
     pickable=True,
     opacity=0.8,
     stroked=True,
@@ -93,22 +46,67 @@ layer = pdk.Layer(
     radius_min_pixels=5,
     radius_max_pixels=15,
     line_width_min_pixels=1,
-    get_position=[['1','2']],
+    get_position=['long', 'lat'],
     #get_radius="exits_radius",
     get_fill_color=[200, 140, 120],
-    get_line_color=[0, 0, 0],
+    get_line_color=[0, 0, 0],)
+    #r.to_html("scatterplot_layer.html")
+    st.write(r01)
+
+
+
+# cls_no=5  ## no. of clusters
+# kmeans=KMeans(n_clusters=cls_no)
+# kmeans.fit(X)
+
+# centroids = kmeans.cluster_centers_
+# labels = kmeans.labels_
+# cluster_counts = np.zeros(cls_no, dtype=int)
+
+# for label in labels:
+#     cluster_counts[label] += 1
+
+# cluster_info = []
+
+# for cluster_label, count, centroid in zip(range(cls_no), cluster_counts, centroids):
+#     cluster_info.append([count,centroid.tolist()[1],centroid.tolist()[0]])
+
+# cl_inf=pd.DataFrame(cluster_info)  #converted data into datframe
+
+
+
+# xt = datetime.now()
+# st.subheader("Data Updated on : ",xt)
+# #####add feature to --like count ,date etc
+
+
+# layer = pdk.Layer(
+#     "ScatterplotLayer",
+#     cl_inf,
+#     pickable=True,
+#     opacity=0.8,
+#     stroked=True,
+#     filled=True,
+#     radius_scale=300,
+#     radius_min_pixels=5,
+#     radius_max_pixels=15,
+#     line_width_min_pixels=1,
+#     get_position=[['1','2']],
+#     #get_radius="exits_radius",
+#     get_fill_color=[200, 140, 120],
+#     get_line_color=[0, 0, 0],
     
-)
+# )
 
 
-tooltip = {
-    "html": "<b>count: ,{0}</b>",
-    "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
-}
+# tooltip = {
+#     "html": "<b>count: ,{0}</b>",
+#     "style": {"background": "grey", "color": "white", "font-family": '"Helvetica Neue", Arial', "z-index": "10000"},
+# }
 
-view_state = pdk.ViewState(longitude=78.348516,
-    latitude=22.824289, zoom=10, bearing=0, pitch=0)
+# view_state = pdk.ViewState(longitude=78.348516,
+#     latitude=22.824289, zoom=10, bearing=0, pitch=0)
 
-r = pdk.Deck(layers=[layer],tooltip=tooltip,initial_view_state=view_state)
-#r.to_html("scatterplot_layer.html")
-st.write(r)
+# r = pdk.Deck(layers=[layer],tooltip=tooltip,initial_view_state=view_state)
+# #r.to_html("scatterplot_layer.html")
+# st.write(r)
